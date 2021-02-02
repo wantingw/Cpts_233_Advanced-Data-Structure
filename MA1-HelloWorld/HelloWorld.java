f91e34389d9865d9e8ea787c5e7c4b4344ec4bb3
jkim4 <jkim4@eecs.wsu.edu>
2021-02-01 20:53:05 -0800

    MA1 merge to master

---
+++ MA1-HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+//
+// CptS 233 - Microassignment #1
+//
+//   Hello World for Git testing
+//   Clone
+//   Edit
+//   	Output should be "Hello World"
+//   Add
+//   Commit
+//   Push
+//
+//
+
+public class HelloWorld {
+    public static void main(String[] args) {
+    }
+}
