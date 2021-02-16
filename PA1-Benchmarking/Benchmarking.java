236e5282264434d3f72cc9d3bf34b81da5d4a3e2
jkim4 <jkim4@eecs.wsu.edu>
2021-02-15 16:00:12 -0800

    PA1 merge to master

---
+++ PA1-Benchmarking/Benchmarking.java
@@ -0,0 +1,16 @@
+/*
+ *  Assignment: Benchmarking a linked list
+ *
+ *  [this file]_main: Main class for the assignment
+ * 
+ *  Copyright:
+ *   For academic use only under the Creative Commons
+ *   Attribution-NonCommercial-NoDerivatives 4.0 International License
+ *   http://creativecommons.org/licenses/by-nc-nd/4.0
+ */
+
+
+class Benchmarking {
+    public static void main(String args[]){
+    }
+}
