931892f996b4e9a7b2318bd22b2be1e9044ddcc4
Wanting Wu <wanting@Wantings-MacBook-Pro.local>
2021-03-25 16:33:37 -0700

    MA1 Re-upload

---
+++ MA1-HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+//
+// CptS 233 - Microassignment #1
+//
+//   Hello World for Git testing
+//   Clone
+//   Edit
+//   	Output should be "Hello World"
+//   Add
+//   Commit
+//   Push
+//
+//
+
+public class HelloWorld {
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+    }
+}
