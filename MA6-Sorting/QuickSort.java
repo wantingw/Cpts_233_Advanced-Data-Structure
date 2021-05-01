962bfeb7d8dd979b633559e064c20fe95750ef10
Wanting Wu <wanting@valley-crest-10-223-92-217.resnet.wsu.edu>
2021-04-30 18:20:29 -0700

    MA6-Sorting Submitted

--- MA6-Sorting/QuickSort.java
+++ MA6-Sorting/QuickSort.java
@@ -15,37 +15,63 @@ public class QuickSort<T extends Comparable<T>> extends Sorter<T> {
     QuickSort() {
         name = "QuickSort";
     }
-/**
- * 
- */
+
     public ArrayList<T> sort(SortStats stats, ArrayList<T> data) {
+      quickSort(stats,data);
+      return data;
+    }
+    
+    
+    private ArrayList<T> quickSort(SortStats stats, ArrayList<T> data){
       if(data.size()>1) {
+        //sublist small for elements smaller than pivot
+        //sublist large for elements larger than pivot
         ArrayList<T> small = new ArrayList<>();
         ArrayList<T> large = new ArrayList<>();
-        T pivot = data.get(0);
-        for(int i = 1; i<data.size(); i++) {
+        
+        int m = data.size()/2;
+        
+        //pick median as a pivot
+        T pivot = data.get(m);
+        for(int i = 0; i<data.size(); i++) {
+          //if not reaching to the median index
+          if(i!=m) {
           T val  = data.get(i);
-          if(val.compareTo(pivot)<0) {
-            small.add(val);
-          }else if (val.compareTo(pivot)>0) {
-            large.add(val);
-          }else {
-            if(i>0) {
+          
+            //elements to the left of pivot
+            if(val.compareTo(pivot)<0) {
+              small.add(val);
+            //elements to the right of pivot  
+            }else if (val.compareTo(pivot)>0) {
               large.add(val);
             }else {
-              small.add(val);
+              //elements with the identical value of pivot
+              //stay at its same side.
+              if(i>0) {
+                large.add(val);
+              }else {
+                small.add(val);
+              }
             }
           }
         }
-        sort(stats,small);
-        sort(stats,large);
-        data.clear();
-        data.addAll(small);
-        data.add(pivot);
-        data.addAll(large);
         
+        ArrayList<T> smallPart = sort(stats,small);
+        ArrayList<T> largePart = sort(stats,large);
+        
+        //set the DATA list
+        setData(data, smallPart, pivot, largePart);
+
       }
       return data;
+      
+    }
+    
+    private void setData(ArrayList<T> data ,ArrayList<T>small, T pivot, ArrayList<T> large) {
+      data.clear();
+      data.addAll(small);
+      data.add(pivot);
+      data.addAll(large);
     }
 
 }
