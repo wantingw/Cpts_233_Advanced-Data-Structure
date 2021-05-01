6a1068437c299dfed8efa7e005e056e0ba96b1b4
Wanting Wu <wanting@valley-crest-10-223-92-217.resnet.wsu.edu>
2021-04-30 23:10:42 -0700

    done

--- MA6-Sorting/QuickSort.java
+++ MA6-Sorting/QuickSort.java
@@ -16,55 +16,55 @@ public class QuickSort<T extends Comparable<T>> extends Sorter<T> {
         name = "QuickSort";
     }
 
-    public ArrayList<T> sort(SortStats stats, ArrayList<T> data) {
-      if(data.size()<=1) {
-        return data;
-      }else {
-        ArrayList<T> small = new ArrayList<T>();
-        ArrayList<T> large = new ArrayList<T>();
-        int mid = data.size()/2;
-        //pick median as a pivot
-        T pivot = data.get(mid);
-        for(int i = 0; i<data.size(); i++) {
-          //if not reaching to the median index
-          if(i!=mid) {
-          T val  = data.get(i);
-          
-            //elements to the left of pivot
-            stats.comparisons++;
-            if(val.compareTo(pivot)<0) {
-              small.add(val);
-            //elements to the right of pivot  
-            }else if (val.compareTo(pivot)>0) {
-              large.add(val);
-            }else {
-              //elements with the identical value of pivot
-              //stay at its same side.
-              if(i>0) {
-                large.add(val);
-              }else {
-                small.add(val);
-              }
-            }
-          }
-        }
-        
-        ArrayList<T> smallPart = sort(stats,small);
-        ArrayList<T> largePart = sort(stats,large);
-        
-        data.clear();
-        //set up the sorted data list
-        for(T left : smallPart) {
-          data.add(left);
-        }
-        data.add(pivot);
-        for(T right : largePart ) {
-          data.add(right);
-        }
-        
-        return data;
-      }
-      
-    }
+  	public ArrayList<T> sort(SortStats stats, ArrayList<T> data) {
+  		if(data.size()<=1) {
+  			return data;
+  		}else {
+  			ArrayList<T> small = new ArrayList<T>();
+  			ArrayList<T> large = new ArrayList<T>();
+  			int mid = data.size()/2;
+  			//pick median as a pivot
+  			T pivot = data.get(mid);
+  			for(int i = 0; i<data.size(); i++) {
+  				//if not reaching to the median index
+  				if(i!=mid) {
+  				T val  = data.get(i);
+  				
+  					//elements to the left of pivot
+  					stats.comparisons++;
+  					if(val.compareTo(pivot)<0) {
+  						small.add(val);
+  					//elements to the right of pivot	
+  					}else if (val.compareTo(pivot)>0) {
+  						large.add(val);
+  					}else {
+  						//elements with the identical value of pivot
+  						//stay at its same side.
+  						if(i>0) {
+  							large.add(val);
+  						}else {
+  							small.add(val);
+  						}
+  					}
+  				}
+  			}
+  			
+  			small = sort(stats,small);
+  			large = sort(stats,large);
+  			
+  			data.clear();
+  			//set up the sorted data list
+  			for(T left : small) {
+  				data.add(left);
+  			}
+  			data.add(pivot);
+  			for(T right : large ) {
+  				data.add(right);
+  			}
+  			
+  			return data;
+  		}
+  		
+  	}
 
 }
